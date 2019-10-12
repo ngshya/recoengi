462164e1d1d43603e129d39663df3289adca84a1
ngshya <ngshya@gmail.com>
2019-10-12 17:35:59 +0200

    Created the unit test for PM class.

--- tests/test.py
+++ tests/test.py
@@ -2,12 +2,18 @@
 # python -m unittest discover tests
 
 import unittest
-import mypkg
+import recoengi
+import recoengi.cf as cf
+import numpy as np
 
-class TestHello(unittest.TestCase):
+class TestCF(unittest.TestCase):
 
     def test_hello(self):
-        self.assertEqual(mypkg.hello(), 'Hello World!')
+        M = np.array([[1,2,3,4], [1, 1, 0, 0], [1, -3, 0, 0.5]])
+        pm = cf.PM(M)
+        pm.computeSimilarityMatrix(bln_bin = False, bln_norm = True, flt_ths = 0.0, ntop = 2)
+
+        self.assertTrue(abs(pm.S.sum() - 3.6035) < 0.01)
 
 
 if __name__ == '__main__':
