42722d9f404415d9b8be53ea0c9f5fa02ea5f721
ngshya <ngshya@gmail.com>
2019-10-12 23:51:27 +0200

    Generate required installs by using pipreqs

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
+with open('recoengi/requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='recoengi',
     url='https://github.com/ngshya/recoengi',
     author='ngshya',
     author_email='ngshya@gmail.com',
     packages=['recoengi'],
-    install_requires=['numpy', 'pandas'],
+    install_requires=required,
     version='0.0.1',
     license='proprietary',
     description='',
