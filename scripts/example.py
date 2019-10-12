f8a19b6c37811a3cfaf63969d8909ab422938893
ngshya <ngshya@gmail.com>
2019-10-12 17:19:49 +0200

    Clean example in example.py

---
+++ scripts/example.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('../')
+import recoengi
+import recoengi.cf as cf
+
+from scipy import sparse
+
+# Simulate 100000 users with their preferences on over 100 products. 
+M = sparse.random(100000, 100, density=0.1, format='csr')
+
+# Initialize PM 
+pm = cf.PM(M)
+# Compute the similarity matrix 
+pm.computeSimilarityMatrix(bln_bin = False, bln_norm = True, flt_ths = 0.0, ntop = 10)
+
