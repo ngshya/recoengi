23bfb4f5eb2d985ff951557090e00a5329b3d9f9
ngshya <ngshya@gmail.com>
2019-10-13 13:42:37 +0200

    Regenerated sample data

--- scripts/generate-sample-data.py
+++ scripts/generate-sample-data.py
@@ -13,7 +13,7 @@ dtf_tmp = dtf_tmp.reset_index(drop = False)
 dtf_tmp = dtf_tmp.rename({"index": "movieId_new"}, axis = 1)
 dtf_ratings = pd.merge(dtf_ratings, dtf_tmp, on = ["movieId"], how = "left").drop(["movieId"], axis=1).rename({"movieId_new": "movieId"}, axis=1)
 dtf_ratings.userId = dtf_ratings.userId-1
-dtf_ratings.rating = (dtf_ratings.rating >= 3) + 0.0
+# dtf_ratings.rating = (dtf_ratings.rating >= 3) + 0.0
 
 tmp_bln_split = np.random.choice([True, False], size=dtf_ratings.shape[0], replace=True, p=[0.8, 0.2])
 dtf_train = dtf_ratings.loc[tmp_bln_split, ["userId", "movieId", "rating"]]
